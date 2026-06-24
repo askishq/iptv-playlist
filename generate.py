3cbb9cf7401e02902c6d9128183b04290a8d45a6
askishq <free16697@gmail.com>
2026-06-24 16:36:23 +0600

    Update channel URLs in generate.py

--- generate.py
+++ generate.py
@@ -1,17 +1,15 @@
 logo = "https://i.postimg.cc/WzMrTGFJ/20260624-154406.jpg"
 
 channels = [
-    ("FIFA 2026 1", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026/index.m3u8?edge-cache-token=Expires=1782373269~Starts=1782286869~URLPrefix=xxx~Signature=yyy"),
-    
-    ("FIFA 2026 2", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-2/index.m3u8?edge-cache-token=Expires=1782373269~Starts=1782286869~URLPrefix=xxx~Signature=yyy"),
+    ("FIFA 2026 1", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026/index.m3u8?edge-cache-token=Expires=1782373269URLPrefix=aHR0cHM6Ly9wcm9kLWNkbjAxLWxpdmUudG9mZmVlbGl2ZS5jb20~Signature=ETR9gt1r4keKvdtFS38PYlOqYSMV1JtcKkctph4wDsnXHvLWV8LlbKqq9hv2BcEJeu2rOjWDxno-CSkTysOxAw"),
 
-    ("FIFA 2026 3", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-3/index.m3u8?edge-cache-token=Expires=1782373269~Starts=1782286869~URLPrefix=xxx~Signature=yyy"),
+    ("FIFA 2026 2", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-2/index.m3u8?edge-cache-token=Expires=1782373269URLPrefix=aHR0cHM6Ly9wcm9kLWNkbjAxLWxpdmUudG9mZmVlbGl2ZS5jb20Starts=1782286869Signature=ETR9gt1r4keKvdtFS38PYlOqYSMV1JtcKkctph4wDsnXHvLWV8LlbKqq9hv2BcEJeu2rOjWDxno-CSkTysOxAw"),
 
-    ("FIFA 2026 4", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-4/index.m3u8?edge-cache-token=Expires=1782373269~Starts=1782286869~URLPrefix=xxx~Signature=yyy"),
+    ("FIFA 2026 4", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-4/index.m3u8?edge-cache-token=Expires=1782373269URLPrefix=aHR0cHM6Ly9wcm9kLWNkbjAxLWxpdmUudG9mZmVlbGl2ZS5jb20~Signature=ETR9gt1r4keKvdtFS38PYlOqYSMV1JtcKkctph4wDsnXHvLWV8LlbKqq9hv2BcEJeu2rOjWDxno-CSkTysOxAw"),
 
-    ("FIFA 2026 5", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-5/index.m3u8?edge-cache-token=Expires=1782373269~Starts=1782286869~URLPrefix=xxx~Signature=yyy"),
+    ("FIFA 2026 5", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-5/index.m3u8?edge-cache-token=Expires=1782373269URLPrefix=aHR0cHM6Ly9wcm9kLWNkbjAxLWxpdmUudG9mZmVlbGl2ZS5jb20~Signature=ETR9gt1r4keKvdtFS38PYlOqYSMV1JtcKkctph4wDsnXHvLWV8LlbKqq9hv2BcEJeu2rOjWDxno-CSkTysOxAw"),
 
-    ("FIFA 2026 6", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-6/index.m3u8?edge-cache-token=Expires=1782373269~Starts=1782286869~URLPrefix=xxx~Signature=yyy"),
+    ("FIFA 2026 6", "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-6/index.m3u8?edge-cache-token=Expires=1782373269URLPrefix=aHR0cHM6Ly9wcm9kLWNkbjAxLWxpdmUudG9mZmVlbGl2ZS5jb20~Signature=ETR9gt1r4keKvdtFS38PYlOqYSMV1JtcKkctph4wDsnXHvLWV8LlbKqq9hv2BcEJeu2rOjWDxno-CSkTysOxAw"),
 ]
 
 with open("playlist.m3u", "w", encoding="utf-8") as f:
@@ -20,5 +18,3 @@ with open("playlist.m3u", "w", encoding="utf-8") as f:
     for name, url in channels:
         f.write(f'#EXTINF:-1 tvg-logo="{logo}",{name}\n')
         f.write(url + "\n\n")
-
-print("Playlist generated")
